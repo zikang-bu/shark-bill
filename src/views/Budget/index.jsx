a5be79357e7419d468c4f64c7c3e59097ee4bd38
卜子康 <442104171@qq.com>
2020-12-23 23:40:20 +0800

    feat: budget module

--- src/views/Budget/index.jsx
+++ src/views/Budget/index.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './index.scss';
 
 class Chart extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>Budget</div>
-         );
-    }
+	state = {};
+	render() {
+		return (
+			<div className="budget">
+				<div className="budget_header">
+					预算
+					<div
+						className="cancel"
+						onClick={() => {
+							window.history.back(-1);
+						}}
+					>
+						取消
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Chart;
